ef1f819400c3af2a192b0268f95fc83f6feaabdb
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-03 16:28:54 +0330

    Update models.py

--- eshop/models.py
+++ eshop/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here.
 
 class Product(models.Model):
     title = models.CharField(max_length=300)
     price = models.IntegerField(max_length=10)
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+
 
     def __str__(self):
         return f"{self.title}, {self.price}s"
