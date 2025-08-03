b167fe4976c453b10116103c6c8b922e10440d77
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-03 16:22:26 +0330

    Update models.py

--- eshop/models.py
+++ eshop/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=300)
     price = models.IntegerField(max_length=10)
+
+    def __str__(self):
+        return f"{self.title}, {self.price}s"
