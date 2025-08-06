3bbcd42e13a5e50e72eeb88e0aa26b6ba509e767
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-06 12:55:25 +0330

    Update views.py

--- eshop/views.py
+++ eshop/views.py
@@ -7,3 +7,7 @@ from eshop.models import Product
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'eshop/product_list.html',{'products':products})
+
+def product_detail(request, productID):
+    product = Product.objects.get(id=productID)
+    return render(request, 'eshop/product_detail.html',{'product':product})
