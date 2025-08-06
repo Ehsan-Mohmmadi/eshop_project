86484c80091be6f8cb45a459191814d056753661
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-06 12:37:08 +0330

    Update views.py

--- eshop/views.py
+++ eshop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from eshop.models import Product
+
+
 # Create your views here.
+def product_list(request):
+    return render(request, 'eshop/product_list.html')
