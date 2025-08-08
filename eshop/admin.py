e1fdf2c2cbb3fafbc6ecce6f46ebad2e5d19e2e3
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-08 21:50:24 +0330

    Update admin.py
    add some options to admin page

--- eshop/admin.py
+++ eshop/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from eshop import models
+from . import models
 
 # Register your models here.
 
-admin.site.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    #readonly_fields = ['slug']
+    prepopulated_fields = { "slug": ["title"]}
+    list_display = ('title', 'price', 'rating' ,'is_active')
+    list_filter = ('is_active', 'rating')
+    list_editable = ['rating', 'is_active']
+
+admin.site.register(models.Product,ProductAdmin)
