ff75392e2615787c42a03f00cd15f1d4b1253cd4
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-10 16:22:14 +0330

    Update admin.py
    activate category in admin

--- eshop/admin.py
+++ eshop/admin.py
@@ -7,8 +7,9 @@ class ProductAdmin(admin.ModelAdmin):
 
     #readonly_fields = ['slug']
     prepopulated_fields = { "slug": ["title"]}
-    list_display = ('title', 'price', 'rating' ,'is_active')
+    list_display = ('title', 'price', 'rating' ,'is_active','category')
     list_filter = ('is_active', 'rating')
     list_editable = ['rating', 'is_active']
 
 admin.site.register(models.Product,ProductAdmin)
+admin.site.register(models.ProductCategory)
