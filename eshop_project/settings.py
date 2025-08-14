d7aad25b9233c0e6d1853df6ce2c8ba94376823a
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-14 15:44:21 +0330

    Adding template
    static files and urls added

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'eshop.apps.EshopConfig',
+    'home_module',
 ]
 
 MIDDLEWARE = [
--- eshop_project/urls.py
+++ eshop_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('eshop.urls')),
+    path('',include('home_module.urls')),
+    path('products', include('eshop.urls')),
+    path('admin/', admin.site.urls)
+
 ]
