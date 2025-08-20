3cd0564f33fb400ba10a2a9be1364e31b6a1a978
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-20 22:11:17 +0330

    Update views.py

--- home_module/views.py
+++ home_module/views.py
@@ -7,3 +7,6 @@ def index_page(request):
 
 def contact_page(request):
     return render(request,'home_module/contact_page.html')
+
+def site_header_partial(request):
+    return render(request,'shared/site_header_partial.html',)
