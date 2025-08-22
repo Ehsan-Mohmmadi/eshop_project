962b4cecc0dd1dc6b80acc22c53630b4e06b717d
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-22 11:07:17 +0330

    Update urls.py

--- eshop_project/urls.py
+++ eshop_project/urls.py
@@ -20,6 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('',include('home_module.urls')),
     path('products/', include('eshop.urls')),
+    path('contact-us/', include('contact_module.urls')),
     path('admin/', admin.site.urls)
 
 ]
