50209b26db7d8c6bc480c608a70f0fc1e9ec672b
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-22 10:46:12 +0330

    footer partial render

--- home_module/views.py
+++ home_module/views.py
@@ -9,4 +9,7 @@ def contact_page(request):
     return render(request,'home_module/contact_page.html')
 
 def site_header_partial(request):
-    return render(request,'shared/site_header_partial.html',)
+    return render(request,'shared/site_header_partial.html')
+
+def site_footer_partial(request):
+    return render(request,'shared/site_footer_partial.html')
