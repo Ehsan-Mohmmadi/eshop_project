81ad9a1c0038010020bb06310bc39be41c41f036
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-22 11:26:47 +0330

    Create ContactModule

--- contact_module/models.py
+++ contact_module/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=100, verbose_name='نام کامل')
+    title = models.CharField(max_length=100, verbose_name='عنوان')
+    email = models.EmailField(verbose_name='ایمیل')
+    message = models.TextField(verbose_name='متن پیام')
+    response = models.TextField(verbose_name='پاسخ')
+    is_read = models.BooleanField(verbose_name='خوانده شده', default=False)
+
+    class Meta:
+        verbose_name = 'پیام'
+        verbose_name_plural = 'پیام ها'
+
+    def __str__(self):
+        return self.title
