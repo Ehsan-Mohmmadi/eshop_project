8d0e56bc2fbe927fbda4d80ebf6e0ceb945903b0
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-23 23:12:37 +0330

    forms

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -1,7 +1,8 @@
-from django import  forms
+from django import forms
+
 
 class ContactForm(forms.Form):
-    full_name = forms.CharField(label="نام و نام خانوادگی")
+    full_name = forms.CharField(label="نام و نام خانوادگی", max_length=50,error_messages={'required': 'Enter your full name'})
     email = forms.EmailField(label="ایمیل")
     subject = forms.CharField(label="موضوع")
     message = forms.CharField(widget=forms.Textarea)
--- contact_module/views.py
+++ contact_module/views.py
@@ -5,14 +5,11 @@ from .forms import ContactForm
 # Create your views here.
 
 def contact_us(request):
-
+    contact_form = ContactForm()
     if request.method == "POST":
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
             print(contact_form.cleaned_data)
             return redirect('home_page')
-    else:
-        contact_form = ContactForm()
 
-    contact_form = ContactForm()
     return render(request, 'contact_module/contact-us.html', {'contact_form': contact_form})
