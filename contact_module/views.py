3afd7a5337f31b9161e678acfe2348126c9f8f17
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-24 13:33:42 +0330

    Model Forms
    send data from form to database

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import ContactForm
+from .models import ContactUs
 
 
 # Create your views here.
@@ -9,7 +10,14 @@ def contact_us(request):
     if request.method == "POST":
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
+            contact = ContactUs(name=contact_form.cleaned_data.get('full_name'),
+                                email=contact_form.cleaned_data.get('email'),
+                                message=contact_form.cleaned_data.get('message'),
+                                title=contact_form.cleaned_data.get('subject'),
+                                is_read=False
+                                )
             print(contact_form.cleaned_data)
+            contact.save()
             return redirect('home_page')
 
     return render(request, 'contact_module/contact-us.html', {'contact_form': contact_form})
