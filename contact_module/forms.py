85b6ecded977927f2133842cc7cc833468d37fd0
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-24 11:37:32 +0330

    Update forms.py
    alert text

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -1,3 +1,5 @@
+from importlib.metadata import requires
+
 from django import forms
 
 
@@ -5,11 +7,12 @@ class ContactForm(forms.Form):
     full_name = forms.CharField(
         label="نام و نام خانوادگی",
         max_length=50,
-        error_messages={'required': 'Enter your full name'},
+        error_messages={'required': 'نام کامل خود را وارد نمایید'},
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'نام و نام خانوادگی'}),
     )
     email = forms.EmailField(
         label="ایمیل",
+        error_messages={'required':'لطفا ایمیل خود را وارد نمایید'},
         widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'ایمیل', })
     )
     subject = forms.CharField(
@@ -17,4 +20,5 @@ class ContactForm(forms.Form):
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'موضوع'})
     )
     message = forms.CharField(
-        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'پیام شما','id':'message'}))
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'پیام شما','id':'message'})
+    )
