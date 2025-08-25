4a2c86446e7e17c7ef9487a9de4bd9e95e9ae320
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-25 13:46:12 +0330

    Update forms.py

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -2,25 +2,25 @@ from django import forms
 from contact_module.models import ContactUs
 
 
-class ContactForm(forms.Form):
-    full_name = forms.CharField(
-        label="نام و نام خانوادگی",
-        max_length=50,
-        error_messages={'required': 'نام کامل خود را وارد نمایید'},
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'نام و نام خانوادگی'}),
-    )
-    email = forms.EmailField(
-        label="ایمیل",
-        error_messages={'required': 'لطفا ایمیل خود را وارد نمایید'},
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'ایمیل', })
-    )
-    title = forms.CharField(
-        label="موضوع",
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'موضوع'})
-    )
-    message = forms.CharField(
-        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'پیام شما', 'id': 'message'})
-    )
+# class ContactForm(forms.Form):
+#     full_name = forms.CharField(
+#         label="نام و نام خانوادگی",
+#         max_length=50,
+#         error_messages={'required': 'نام کامل خود را وارد نمایید'},
+#         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'نام و نام خانوادگی'}),
+#     )
+#     email = forms.EmailField(
+#         label="ایمیل",
+#         error_messages={'required': 'لطفا ایمیل خود را وارد نمایید'},
+#         widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'ایمیل', })
+#     )
+#     title = forms.CharField(
+#         label="موضوع",
+#         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'موضوع'})
+#     )
+#     message = forms.CharField(
+#         widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'پیام شما', 'id': 'message'})
+#     )
 
 
 class ContactUsModelForm(forms.ModelForm):
