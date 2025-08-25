5e7cda1038ea862f19cab1894f23075b856e2107
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-25 09:53:21 +0330

    use modelforms

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -1,6 +1,5 @@
-from importlib.metadata import requires
-
 from django import forms
+from contact_module.models import ContactUs
 
 
 class ContactForm(forms.Form):
@@ -12,13 +11,25 @@ class ContactForm(forms.Form):
     )
     email = forms.EmailField(
         label="ایمیل",
-        error_messages={'required':'لطفا ایمیل خود را وارد نمایید'},
+        error_messages={'required': 'لطفا ایمیل خود را وارد نمایید'},
         widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'ایمیل', })
     )
-    subject = forms.CharField(
+    title = forms.CharField(
         label="موضوع",
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'موضوع'})
     )
     message = forms.CharField(
-        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'پیام شما','id':'message'})
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'پیام شما', 'id': 'message'})
     )
+
+
+class ContactUsModelForm(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = ['name', 'email', 'title', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'نام و نام خانوادگی'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'ایمیل'}),
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'موضوع'}),
+            'message': forms.Textarea(attrs={'class':'form-control', 'placeholder':'متن پیام', 'id': 'message'})
+        }
--- contact_module/views.py
+++ contact_module/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, redirect
-from .forms import ContactForm
+from .forms import ContactForm, ContactUsModelForm
 from .models import ContactUs
 # Create your views here.
 
 def contact_us(request):
-    contact_form = ContactForm()
-    if request.method == "POST":
-        contact_form = ContactForm(request.POST)
-        if contact_form.is_valid():
-            contact = ContactUs(name=contact_form.cleaned_data.get('full_name'),
-                                email=contact_form.cleaned_data.get('email'),
-                                message=contact_form.cleaned_data.get('message'),
-                                title=contact_form.cleaned_data.get('subject'),
-                                is_read=False
-                                )
-            print(contact_form.cleaned_data)
-            contact.save()
-            return redirect('home_page')
+    #contact_form = ContactForm()
+    contact_form = ContactUsModelForm()
+    #contact = ContactUsModelForm(request.POST)
 
+    if request.method == "POST":
+        contact_form = ContactUsModelForm(request.POST)
+        if contact_form.is_valid():
+            # contact = ContactUs(full_name=contact_form.cleaned_data.get('full_name'),
+            #                     email=contact_form.cleaned_data.get('email'),
+            #                     message=contact_form.cleaned_data.get('message'),
+            #                     title=contact_form.cleaned_data.get('title'),
+            #                     is_read=False
+            #                     )
+            # print(contact_form.cleaned_data)
+            # contact.save()
+            contact_form.save()
+            return redirect('home_page')
     return render(request, 'contact_module/contact-us.html', {'contact_form': contact_form})
