a90b4201e437cbff799f0aebd1b50f03e22e28bf
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-25 14:07:46 +0330

    template view

--- home_module/views.py
+++ home_module/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
+    
 
 # Create your views here.
 
-def index_page(request):
-    return render(request,'home_module/index_page.html')
+# class HomePageView(View):
+#     def get(self, request):
+#         return render(request, 'home_module/index_page.html')
+
+class HomePageView(TemplateView):
+    template_name = 'home_module/index_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = 'This is data'
+
 
 def site_header_partial(request):
-    return render(request,'shared/site_header_partial.html')
+    return render(request, 'shared/site_header_partial.html')
+
 
 def site_footer_partial(request):
-    return render(request,'shared/site_footer_partial.html')
+    return render(request, 'shared/site_footer_partial.html')
